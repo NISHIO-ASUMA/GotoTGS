27f67e463509ec3f5163c7426133498746e367fb
ShouyaChikada <241313@st.yoshida-g.ac.jp>
2026-04-22 10:32:37 +0900

    [Update] プレイヤークラスを書きました

--- src/include/player.h
+++ src/include/player.h
@@ -9,3 +9,41 @@
 // インクルードガード
 //*********************************************************
 #pragma once
+
+//*********************************************************
+// インクルードファイル
+//*********************************************************
+#include "movecharactor.h"
+
+//*********************************************************
+// 前方宣言
+//*********************************************************
+class CBoxCollider;
+
+//*********************************************************
+// プレイヤーオブジェクトクラスを定義
+//*********************************************************
+class CPlayer : public CMoveCharactor
+{
+public:
+	CPlayer(int nPriority = static_cast<int>(CObject::PRIORITY::CHARACTOR));
+	~CPlayer();
+
+	HRESULT Init(void) override;
+	void Uninit(void) override;
+	void Update(void) override;
+	void Draw(void) override;
+
+	/// <summary>
+	/// プレイヤー生成処理
+	/// </summary>
+	/// <param name="pos">生成座標</param>
+	/// <param name="rot">角度</param>
+	/// <returns></returns>
+	static CPlayer* Create(
+		const D3DXVECTOR3& pos,
+		const D3DXVECTOR3& rot
+	);
+private:
+	std::unique_ptr<CBoxCollider> m_pCollider;	// 矩形のコライダー
+};
