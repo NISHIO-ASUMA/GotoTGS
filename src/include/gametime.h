599ac72f479660a4b828f4e6c38333d016e22f29
髙橋美咲(takahashi misaki) <241311@st.yoshida-g.ac.jp>
2026-04-24 12:16:50 +0900

    [update] タイムの処理の更新とその他修正

--- src/include/gametime.h
+++ src/include/gametime.h
@@ -16,6 +16,11 @@
 #include "object.h"
 
 //*********************************************************
+// 前方宣言
+//*********************************************************
+class CNumber;
+
+//*********************************************************
 // ゲームのタイマーに対するクラスを定義
 //*********************************************************
 class CGametime :public CObject
@@ -31,9 +36,27 @@ public:
 	void Draw(void);
 
 	// 生成処理
-	static CGametime* Create(void);
+	static CGametime* Create(const D3DXVECTOR3& pos,float fWidth, float fHeight);
 
 private:
 
+	// 定数構造体宣言
+	struct Config
+	{
+		static constexpr int TIMEDIGIT = 4;					// 表示桁数
+		static constexpr int TIMELIMIT = 180;				// 制限時間
+		static constexpr int UNIT = 60;						// 単位
+		static constexpr int DIGITNUM = 10;					// 分割桁数
+		static constexpr float POS_Y = 50.0f;				// Y座標値
+		static constexpr float POS_WIDTH_VALUE = 2.0f;		// ずらす横幅乗算
+		static constexpr const char* TEXNAME = "time.png";	// テクスチャ名
+	};
+
 	// メンバ変数
+	D3DXVECTOR3 m_pos;							// 位置
+	float m_fWidth;								// 横幅
+	float m_fHeight;							// 縦幅
+	int m_NowTime;								// 残り時間
+	int m_nCountFream;							// フレーム数
+	CNumber* m_pNumber[Config::TIMEDIGIT];		// 桁数
 };
