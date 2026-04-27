6c869cd585c40ece85c78d314cde5136d5bf252f
髙橋美咲(takahashi misaki) <241311@st.yoshida-g.ac.jp>
2026-04-27 11:29:13 +0900

    [update] タイマーの処理更新

--- src/cpp/gametime.cpp
+++ src/cpp/gametime.cpp
@@ -19,18 +19,22 @@
 //=========================================================
 // コンストラクタ
 //=========================================================
-CGametime::CGametime(int nPriority) : CObject(nPriority)
+CGametime::CGametime(int nPriority) : CObject(nPriority),
+m_nAllTime(NULL),
+m_nSeconds(NULL),
+m_nMinutes(NULL),
+m_nDecTime(NULL),
+m_nCounter(NULL),
+m_fWidth(NULL),
+m_fHeight(NULL),
+m_pos(VECTOR3_NULL)
 {
-	// 値を初期化
-	m_pos = VECTOR3_NULL;
-	m_fWidth = 0.0f;
-	m_fHeight = 0.0f;
-	m_NowTime = 0;
 
-	for (int nDigit = 0; nDigit < Config::TIMEDIGIT; nDigit++)
+	for (int nDigit = 0; nDigit < Config::DIGIT_TIME; nDigit++)
 	{
-		m_pNumber[nDigit] = nullptr;
-	}
+		m_pNumberMinutes[nDigit] = nullptr;
+		m_pNumberSeconds[nDigit] = nullptr;
+	}	
 }
 
 //=========================================================
@@ -38,8 +42,6 @@ CGametime::CGametime(int nPriority) : CObject(nPriority)
 //=========================================================
 CGametime::~CGametime()
 {
-
-
 }
 
 //=========================================================
@@ -55,10 +57,10 @@ CGametime* CGametime::Create(const D3DXVECTOR3& pos, float fWidth, float fHeight
 		return nullptr;
 	}
 
-	// 引数をメンバ変数に代入
-	pGametime->m_pos = pos;
-	pGametime->m_fWidth = fWidth;
-	pGametime->m_fHeight = fHeight;
+	// 引数を設定
+	pGametime->SetPos(pos);
+	pGametime->SetWidth(fWidth);
+	pGametime->SetHeight(fHeight);
 
 	if (FAILED(pGametime->Init()))
 	{// 初期化が失敗した場合
@@ -72,32 +74,61 @@ CGametime* CGametime::Create(const D3DXVECTOR3& pos, float fWidth, float fHeight
 //=========================================================
 HRESULT CGametime::Init(void)
 {
-	// 一桁の横幅
-	float fTexpos = m_fWidth / Config::TIMEDIGIT;
+	// 全体の時間を設定
+	m_nAllTime = Config::NUMTIME;
 
-	// 桁数分
-	for (int nDigit = 0; nDigit < Config::TIMEDIGIT; nDigit++)
+	// 現在の分数を計算
+	m_nMinutes = m_nAllTime / Config::CARVETIME;
+
+	// 現在の秒数を計算
+	m_nSeconds = m_nAllTime % Config::CARVETIME;
+
+	// 一桁の横幅
+	float fTexpos = m_fWidth / Config::DIGIT_TIME;
+
+	// 分を生成
+	for (int nDigit = 0; nDigit < Config::DIGIT_TIME; nDigit++)
 	{
-		// インスタンス生成
-		m_pNumber[nDigit] = new CNumber;
+		m_pNumberMinutes[nDigit] = new CNumber;
 
 		// ナンバーの初期化処理
-		m_pNumber[nDigit]->Init(D3DXVECTOR3(m_pos.x - (fTexpos * Config::POS_WIDTH_VALUE * nDigit), Config::POS_Y, 0.0f),		// 位置
-			fTexpos,																											// 一桁分の横幅
-			m_fHeight);																											// 縦幅
+		m_pNumberMinutes[nDigit]->Init(D3DXVECTOR3(m_pos.x + (fTexpos * Config::VALUE_FLOAT * nDigit), m_pos.y, 0.0f),		// 位置
+			fTexpos,																										// 一桁分の横幅
+			m_fHeight);																										// 縦幅
 
 		// サイズ設定
-		m_pNumber[nDigit]->SetSize(fTexpos, m_fHeight);
+		m_pNumberMinutes[nDigit]->SetSize(fTexpos, m_fHeight);
 
 		// カラー設定
-		m_pNumber[nDigit]->SetCol(COLOR_WHITE);
+		m_pNumberMinutes[nDigit]->SetCol(COLOR_WHITE);
 
 		// テクスチャ設定
-		m_pNumber[nDigit]->SetTexture(Config::TEXNAME);
+		m_pNumberMinutes[nDigit]->SetTexture(Config::TEXNAME);
 	}
 
-	// 制限時間を設定
-	m_NowTime = Config::TIMELIMIT;
+	// 位置を横にずらす
+	m_pos.x += Config::VALUE_WIDTH;
+
+	// 秒を生成
+	for (int nDigit = 0; nDigit < Config::DIGIT_TIME; nDigit++)
+	{
+		m_pNumberSeconds[nDigit] = new CNumber;
+
+		// ナンバーの初期化処理
+		m_pNumberSeconds[nDigit]->Init(D3DXVECTOR3(m_pos.x + (fTexpos * Config::VALUE_FLOAT * nDigit), m_pos.y, 0.0f),		// 位置
+			fTexpos,																										// 一桁分の横幅
+			m_fHeight);																										// 縦幅
+
+		// サイズ設定
+		m_pNumberSeconds[nDigit]->SetSize(fTexpos, m_fHeight);
+
+		// カラー設定
+		m_pNumberSeconds[nDigit]->SetCol(COLOR_WHITE);
+
+		// テクスチャ設定
+		m_pNumberSeconds[nDigit]->SetTexture(Config::TEXNAME);
+
+	}
 
 	return S_OK;
 }
@@ -108,15 +139,24 @@ HRESULT CGametime::Init(void)
 void CGametime::Uninit(void)
 {
 	// 桁数分破棄
-	for (int nDigit = 0; nDigit < Config::TIMEDIGIT; nDigit++)
+	for (int nDigit = 0; nDigit < Config::DIGIT_TIME; nDigit++)
 	{
-		if (m_pNumber[nDigit] != nullptr)
+		if (m_pNumberMinutes[nDigit] != nullptr)
 		{
-			// ナンバーの終了処理
-			m_pNumber[nDigit]->Uninit();
-			delete m_pNumber[nDigit];
-			m_pNumber[nDigit] = nullptr;
+			// 分数の終了処理
+			m_pNumberMinutes[nDigit]->Uninit();
+			delete m_pNumberMinutes[nDigit];
+			m_pNumberMinutes[nDigit] = nullptr;
 		}
+
+		if (m_pNumberSeconds[nDigit] != nullptr)
+		{
+			// 秒数の終了処理
+			m_pNumberSeconds[nDigit]->Uninit();
+			delete m_pNumberSeconds[nDigit];
+			m_pNumberSeconds[nDigit] = nullptr;
+		}
+
 	}
 
 	// オブジェクト自身の破棄
@@ -128,31 +168,45 @@ void CGametime::Uninit(void)
 //=========================================================
 void CGametime::Update(void)
 {
-	if (m_nCountFream < Config::UNIT)
+	if (m_nCounter <= 0)
 	{
-		// フレームを1つ増やす
-		m_nCountFream++;
+		// カウンターを0にする
+		m_nCounter = 0;
 
 		return;
 	}
 
-	// フレームを0にする
-	m_nCountFream = 0;
+	// カウンターを1つ増やす
+	m_nCounter++;
 
-	// 残り時間を減らす
-	m_NowTime--;
+	if (m_nCounter >= Config::CARVETIME)
+	{// カウンターが1Fを超えたとき
+		
+		// カウンターを0にする
+		m_nCounter = 0;
 
-	// 残り時間
-	int nNowTime = ((m_NowTime / Config::UNIT) * Config::DIGITNUM * Config::DIGITNUM) + (m_NowTime % Config::UNIT);
+		// 全体の時間を減らす
+		m_nAllTime--;
 
-	for (int nDigit = 0; nDigit < Config::TIMEDIGIT; nDigit++)
-	{
-		// 1桁を取り出す
-		int nNum = 0;
+		// 現在の分数を計算
+		m_nMinutes = m_nAllTime / Config::CARVETIME;
 
-		// 桁を更新する
-		m_pNumber[nDigit]->SetDigit(nNum);
+		// 現在の秒数を計算
+		m_nSeconds = m_nAllTime % Config::CARVETIME;
+
+		if (m_nAllTime <= 0)
+		{// 現在の時間が0以下になった時
+			// カウンターを0にする
+			m_nCounter = 0;
+		}
+
+		// 経過時間を増やす
+		m_nDecTime++;
 	}
+
+	// それぞれの数字の計算処理
+	Seconds();
+	Minutes();
 }
 
 //=========================================================
@@ -161,9 +215,58 @@ void CGametime::Update(void)
 void CGametime::Draw(void)
 {
 	// 桁数分表示
-	for (int nDigit = 0; nDigit < Config::TIMEDIGIT; nDigit++)
+	for (int nDigit = 0; nDigit < Config::DIGIT_TIME; nDigit++)
 	{
 		// ナンバーの描画処理
-		m_pNumber[nDigit]->Draw();
+		m_pNumberMinutes[nDigit]->Draw();
+		m_pNumberSeconds[nDigit]->Draw();
+
 	}
+}
+
+//=========================================================
+// 秒数処理
+//=========================================================
+void CGametime::Seconds(void)
+{
+	// 桁数ごとの分割値
+	int nTimeDate = Config::DIVIDE * Config::DIVIDE;
+	int nTimeDateBase = Config::DIVIDE;
+
+	for (int nDigit = 0; nDigit < Config::DIGIT_TIME; nDigit++)
+	{
+		int nPosTexU = m_nSeconds % nTimeDate / nTimeDateBase;
+		nTimeDate /= Config::DIVIDE;
+		nTimeDateBase /= Config::DIVIDE;
+
+		if (m_pNumberSeconds[nDigit] != nullptr)
+		{// 秒数の更新処理
+			m_pNumberSeconds[nDigit]->Update();
+			m_pNumberSeconds[nDigit]->SetDigit(nPosTexU);
+		}
+	}
+}
+
+//=========================================================
+// 分数処理
+//=========================================================
+void CGametime::Minutes(void)
+{
+	// 桁数ごとの分割値
+	int nTimeDate = Config::DIVIDE * Config::DIVIDE;
+	int nTimeDateBase = Config::DIVIDE;
+
+	for (int nDigit = 0; nDigit < Config::DIGIT_TIME; nDigit++)
+	{
+		int nPosTexU = m_nMinutes % nTimeDate / nTimeDateBase;
+		nTimeDate /= Config::DIVIDE;
+		nTimeDateBase /= Config::DIVIDE;
+
+		if (m_pNumberMinutes[nDigit] != nullptr)
+		{// 分数の更新処理
+			m_pNumberMinutes[nDigit]->Update();
+			m_pNumberMinutes[nDigit]->SetDigit(nPosTexU);
+		}
+	}
+
 }
--- src/include/gametime.h
+++ src/include/gametime.h
@@ -21,7 +21,7 @@
 class CNumber;
 
 //*********************************************************
-// ゲームのタイマーに対するクラスを定義
+// タイマークラスを定義
 //*********************************************************
 class CGametime :public CObject
 {
@@ -30,33 +30,57 @@ public:
 	CGametime(int nPriority = static_cast<int>(CObject::PRIORITY::UI));
 	~CGametime();
 
-	HRESULT Init(void);
-	void Uninit(void);
-	void Update(void);
-	void Draw(void);
+	HRESULT Init(void) override;
+	void Uninit(void) override;
+	void Update(void) override;
+	void Draw(void) override;
+
+	// 各種設定処理
+	inline void SetPos(const D3DXVECTOR3& pos) { m_pos = pos; }
+	inline void SetWidth(const float& fWidth) { m_fWidth = fWidth; }
+	inline void SetHeight(const float& fHeight) { m_fHeight = fHeight; }
+
+	// 情報取得処理
+	inline int GetAllTime(void) const { return m_nAllTime; }
+	inline int GetNowTime(void) const { return m_nSeconds; }
 
 	// 生成処理
 	static CGametime* Create(const D3DXVECTOR3& pos,float fWidth, float fHeight);
 
 private:
 
+	//************************************
 	// 定数構造体宣言
+	//************************************
 	struct Config
 	{
-		static constexpr int TIMEDIGIT = 4;					// 表示桁数
-		static constexpr int TIMELIMIT = 180;				// 制限時間
-		static constexpr int UNIT = 60;						// 単位
-		static constexpr int DIGITNUM = 10;					// 分割桁数
-		static constexpr float POS_Y = 50.0f;				// Y座標値
-		static constexpr float POS_WIDTH_VALUE = 2.0f;		// ずらす横幅乗算
-		static constexpr const char* TEXNAME = "time.png";	// テクスチャ名
+		static constexpr float VALUE_WIDTH = 150.0f;			// 横幅の増加分
+		static constexpr float VALUE_FLOAT = 2.0f;				// ずらす基準値
+		static constexpr const char* TEXNAME = "time.png";		// テクスチャ名
+		static constexpr int DIGIT_TIME = 2;					// 桁数
+		static constexpr int DIVIDE = 10;						// 桁分割の値
+		static constexpr int CARVETIME = 60;					// カウント上限
+#ifdef _DEBUG
+		static constexpr int NUMTIME = 600;						// 最大タイマー
+#else
+		static constexpr int NUMTIME = 150;						// 最大タイマー
+#endif // _DEBUG
 	};
 
+	// メンバ関数
+	void Seconds(void);
+	void Minutes(void);
+
 	// メンバ変数
-	D3DXVECTOR3 m_pos;							// 位置
-	float m_fWidth;								// 横幅
-	float m_fHeight;							// 縦幅
-	int m_NowTime;								// 残り時間
-	int m_nCountFream;							// フレーム数
-	CNumber* m_pNumber[Config::TIMEDIGIT];		// 桁数
+	int m_nAllTime;		// 全体の時間
+	int m_nSeconds;		// 現在の時間（秒数）
+	int m_nMinutes;		// 現在の時間（分数）
+	int m_nDecTime;		// 経過時間
+	int m_nCounter;		// カウンター
+	float m_fWidth;		// 横幅
+	float m_fHeight;	// 縦幅
+
+	CNumber* m_pNumberMinutes[Config::DIGIT_TIME];		// 分数のポインタ
+	CNumber* m_pNumberSeconds[Config::DIGIT_TIME];		// 秒数のポインタ
+	D3DXVECTOR3 m_pos;									// 位置
 };
