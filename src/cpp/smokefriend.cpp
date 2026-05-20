0fc81c338d5019cd533a7249909a458d54338320
NISHIO-ASUMA <241315@st.yoshida-g.ac.jp>
2026-05-20 10:44:10 +0900

    [ update ] 煙草の同僚設計

--- src/cpp/smokefriend.cpp
+++ src/cpp/smokefriend.cpp
@@ -3,6 +3,9 @@
 // 煙草吸っている同僚の処理 [ smokefriend.cpp ]
 // Author: Asuma Nishio
 // 
+// 
+// 煙草のアクションモーションを繰り返す回数でモーションを変更するか決めたい
+// 
 //========================================================
 
 //*********************************************************
@@ -27,9 +30,10 @@
 namespace SMOKEFRIEND
 {
 	constexpr const char* SCRIPT = "data/MOTION/Smoke/SmokeMotion.txt"; // モーションファイル
-	constexpr int MIN_CHANGETIME = 600;									// 10秒
-	constexpr int MAX_CHANGETIME = 1200;								// 20秒
 	constexpr int BLEND = 5;											// ブレンド
+	constexpr int MIN_CHANGETIME = 6;
+	constexpr int MAX_CHANGETIME = 8;
+	constexpr int MAX_COUNT = 120;
 };
 
 //========================================================
@@ -37,7 +41,9 @@ namespace SMOKEFRIEND
 //========================================================
 CSmokeFriend::CSmokeFriend(int nPriority) : CNoMoveCharactor(nPriority),
 m_nChangeCount(NULL),
-m_nNextChangeCount(NULL)
+m_nNextChangeCount(NULL),
+m_nLoopCount(NULL),
+m_nTargetLoopCount(NULL)
 {
 
 }
@@ -79,9 +85,6 @@ HRESULT CSmokeFriend::Init(void)
 	// モーションロード
 	MotionLoad(SMOKEFRIEND::SCRIPT, MOTION::MAX,false);
 
-	// 初期ランダム関数
-	RandomChange();
-
 	return S_OK;
 }
 //========================================================
@@ -97,22 +100,46 @@ void CSmokeFriend::Uninit(void)
 //========================================================
 void CSmokeFriend::Update(void)
 {
-	// カウント加算
-	m_nChangeCount++;
+	// 現在のモーション番号を取得
+	int nMotionType = GetMotion()->GetMotionType();
 
-	// 超過したら
-	if (m_nChangeCount >= m_nNextChangeCount)
+	// 待機状態
+	if (nMotionType == MOTION::NEUTRAL)
 	{
-		// モーションの番号を取得
-		int nMotionType = GetMotion()->GetMotionType();
-		int nNextMotion = (nMotionType == MOTION::NEUTRAL) ? MOTION::ACTION : MOTION::NEUTRAL;
+		m_nChangeCount++;
 
-		// モーション変更
-		GetMotion()->SetMotion(nNextMotion,true,SMOKEFRIEND::BLEND);
+		if (m_nChangeCount >= SMOKEFRIEND::MAX_COUNT)
+		{
+			// タバコモーションへ移行
+			GetMotion()->SetMotion(MOTION::ACTION, true, SMOKEFRIEND::BLEND);
 
-		// 変数初期化
-		m_nChangeCount = 0;
-		RandomChange();
+			// 変数の初期化
+			m_nChangeCount = 0;
+			m_nLoopCount = 0;
+
+			// タバコを何回繰り返すかをランダムで決定
+			static std::random_device rd;
+			static std::mt19937 gen(rd());
+			std::uniform_int_distribution<> dis(SMOKEFRIEND::MIN_CHANGETIME, SMOKEFRIEND::MAX_CHANGETIME);
+			m_nTargetLoopCount = dis(gen);
+		}
+	}
+	else if (nMotionType == MOTION::ACTION)
+	{
+		if (GetMotion()->GetFinishMotion())
+		{
+			m_nLoopCount++; // ループ回数を加算
+
+			// リセット
+			GetMotion()->SetFinishMotion(false);
+
+			// 目標回数に達したらニュートラルに戻る
+			if (m_nLoopCount >= m_nTargetLoopCount)
+			{
+				// モーション変更
+				GetMotion()->SetMotion(MOTION::NEUTRAL, true, SMOKEFRIEND::BLEND);
+			}
+		}
 	}
 
 	// 親クラスの更新処理
@@ -125,17 +152,4 @@ void CSmokeFriend::Draw(void)
 {
 	// 親クラスの描画処理
 	CNoMoveCharactor::Draw();
-}
-//========================================================
-// ランダム変更計算
-//========================================================
-void CSmokeFriend::RandomChange(void)
-{
-	// ランダムを生成する
-	static std::random_device rd;
-	static std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis(SMOKEFRIEND::MIN_CHANGETIME, SMOKEFRIEND::MAX_CHANGETIME);
-	
-	// 変更カウントに設定
-	m_nNextChangeCount = dis(gen);
 }
