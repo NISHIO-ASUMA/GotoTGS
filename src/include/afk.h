8185ffb5025cb68df1c81540bdad7eb2928fadc4
ShouyaChikada <241313@st.yoshida-g.ac.jp>
2026-05-27 12:09:42 +0900

    [add] さぼるの親クラスとたばこの処理追加

---
+++ src/include/afk.h
@@ -0,0 +1,48 @@
+//=========================================================
+//
+// AFK処理 [ afk.h ]
+// Author: Shouya Chikada
+//
+//=========================================================
+
+//*********************************************************
+// インクルードガード
+//*********************************************************
+#pragma once
+
+//*********************************************************
+// システムインクルードファイル
+//*********************************************************
+#include <memory>
+
+//*********************************************************
+// 前方宣言
+//*********************************************************
+class CSphereCollider;
+
+//*********************************************************
+// ブロックオブジェクトクラスを定義
+//*********************************************************
+class CAfk
+{
+public:
+
+	CAfk();
+	~CAfk();
+
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	bool Collision(CSphereCollider* pOther);
+
+	inline CSphereCollider* GetCollider(void) { return m_pCollider.get(); }
+
+	void SetRadius(float fRadius) { m_fRadius = fRadius; }
+	void SetPos(D3DXVECTOR3 pos) { m_pos = pos; }
+
+private:
+	std::unique_ptr<CSphereCollider> m_pCollider;	// 球形のコライダー
+	D3DXVECTOR3 m_pos;								// 位置
+	float m_fRadius;								// UI表示範囲の半径用変数
+};
+
